8c9d8fb8fbfe2c5c169c24c9bff43978129498d5
ilyharaksher <ilyharakshw@gmail.com>
2026-08-18 16:07:54 +0300

    Finished validation and generating ValuesList and result

--- Services/CsvImportService.cs
+++ Services/CsvImportService.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
-using System.Globalization;
+﻿using System.Globalization;
 using Web_Api_Timescale_Data.Entities;
 using Web_Api_Timescale_Data.Exceptions;
+using Web_Api_Timescale_Data.Data;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Web_Api_Timescale_Data.Services
 {
     public class CsvImportService : ICsvImportService
     {
+        private readonly ApplicationDbContext _dbContext;
+        public CsvImportService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
         public async Task ImportAsync(IFormFile file)
         {
             using var stream = file.OpenReadStream();
@@ -24,22 +33,13 @@ namespace Web_Api_Timescale_Data.Services
             var line = await reader.ReadLineAsync();
             int lineNumber = 1;
 
-            // проверка структуры строки
-            // пробразование типов
-            // проверка значений согласно файлу
-            // добавить ValueEntity в коллекцию
-            // проверка кол-ва ValueEntity (добавить проверку длины списка в while)
-            // посчитать ResultEntity
-            // .db.SaveChanges();
-
-            // spisok значений, который будем кидать в табличку values
             var ValuesList = new List<ValueEntity>();
 
-            while (line != null) // && ValuesList.Count < 10000
+            while (line != null)
             {
-                if (ValuesList.Count > 10000)
+                if (lineNumber > 10000)
                 {
-                    throw new CsvValidationException("В файле более 10000 строк");            
+                    throw new CsvValidationException("Количество записей в файле не может быть больше 10000");
                 }
 
                 string[] parts = line.Split(";");
@@ -49,10 +49,9 @@ namespace Web_Api_Timescale_Data.Services
                     throw new CsvValidationException($"Ошибка в Записи {lineNumber}: Запись должна содержать Date, ExecutionTime, Value");
                 }
 
-                // проверка типа для Date
                 if (!DateTime.TryParseExact(
                     parts[0],
-                    "yyyy-MM-dd'T'HH-mm-ss-ffff'Z'",
+                    "yyyy-MM-dd'T'HH:mm:ss.ffff'Z'",
                     CultureInfo.InvariantCulture,
                     DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal,
                     out DateTime date
@@ -61,26 +60,29 @@ namespace Web_Api_Timescale_Data.Services
                     throw new CsvValidationException($"Ошибка в Записи {lineNumber}: Неверный формат даты");
                 }
 
-                // проверка типа для ExecutionTime
+                if (date > DateTime.Now || date < new DateTime(2000, 1, 1))
+                {
+                    throw new CsvValidationException($"Ошибка в Записи {lineNumber}: Дата не может быть позже текущей и раньше 01.01.2000");
+                }
+
                 if (!double.TryParse(parts[1], out double executionTime))
                 {
                     throw new CsvValidationException($"Ошибка в Записи {lineNumber}: Неверный формат ExecutionTime");
                 }
-                // проверка значения ExecutionTime
+
                 if (executionTime < 0)
                 {
-                    return; // ошибка значения ExecutionTime
+                    throw new CsvValidationException($"Ошибка в Записи {lineNumber}: Время выполнения не может быть меньше 0");
                 }
 
-                // проверка типа для Value
                 if (!double.TryParse(parts[2], out double value))
                 {
-                    return; // ошибка типа для Value
+                    throw new CsvValidationException($"Ошибка в Записи {lineNumber}: Неверный формат Value");
                 }
 
                 if (value < 0)
                 {
-                    return; // ошибка значения Value
+                    throw new CsvValidationException($"Ошибка в Записи {lineNumber}: Value не может быть меньше 0");
                 }
 
                 ValueEntity measure = new ValueEntity
@@ -92,9 +94,44 @@ namespace Web_Api_Timescale_Data.Services
                 };
 
                 ValuesList.Add(measure);
-
+                lineNumber++;
                 line = await reader.ReadLineAsync();
             }
+
+            if (ValuesList.Count < 1)
+            {
+                throw new CsvValidationException("Количество записей в файле не может быть меньше 1");
+            }
+
+
+            DateTime minDate = ValuesList.Min(x => x.Date);
+            DateTime maxDate = ValuesList.Max(x => x.Date);
+            Double delta = (maxDate - minDate).TotalSeconds;
+            Double avgExecTime = ValuesList.Average(x => x.ExecutionTime);
+            Double avgValue = ValuesList.Average(x => x.Value);
+            Double maxValue = ValuesList.Max(x => x.Value);
+            Double minValue = ValuesList.Min(x => x.Value);
+            
+            var sorted = ValuesList.OrderBy(x => x.Value).ToList();
+            int length = sorted.Count();
+            Double medianValue = length % 2 == 0
+                ? (sorted[length / 2].Value + sorted[length / 2 - 1].Value) / 2 
+                : sorted[length / 2].Value;
+
+
+            ResultEntity result = new ResultEntity
+            {
+                FileName = fileName,
+                Delta = delta,
+                MinDate = minDate,
+                AvgExecutionTime = avgExecTime,
+                AvgValue = avgValue,
+                MedianValue = medianValue,
+                MaxValue = maxValue,
+                MinValue = minValue
+            };
+
+            
         }
     }
 }
