a6e8e9e003c1ef9e97b8a90f06a3a664c196eeed
ilyharaksher <ilyharakshw@gmail.com>
2026-08-25 14:47:02 +0300

    Added ResultExportController that allows to get records from the "Results" table of the database using filters.
    You can filter the records by filename or by ranging start date, avg execution time or avg value.

---
+++ Controllers/ResultsExportController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web_Api_Timescale_Data.Data;
+using Web_Api_Timescale_Data.DTO;
+using Web_Api_Timescale_Data.Entities;
+
+
+
+namespace Web_Api_Timescale_Data.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResultsExportController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public ResultsExportController(ApplicationDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetResults([FromQuery] ResultFilterDTO filter)
+        {
+            IQueryable<ResultEntity> query = _dbContext.Results;
+            if (filter.FileName != null)
+            {
+                query = query.Where(x => x.FileName == filter.FileName);
+            }
+            if (filter.MinStartDate.HasValue)
+            {
+                query = query.Where(x => x.MinDate >= filter.MinStartDate);
+            }
+            if (filter.MaxStartDate.HasValue)
+            {
+                query = query.Where(x => x.MinDate <= filter.MaxStartDate);
+            }
+            if (filter.MinAvgValue.HasValue)
+            {
+                query = query.Where(x => x.AvgValue >= filter.MinAvgValue);
+            }
+            if (filter.MaxAvgValue.HasValue)
+            {
+                query = query.Where(x => x.AvgValue <= filter.MaxAvgValue);
+            }
+            if (filter.MinAvgTime.HasValue)
+            {
+                query = query.Where(x => x.AvgExecutionTime >= filter.MinAvgTime);
+            }
+            if (filter.MaxAvgTime.HasValue)
+            {
+                query = query.Where(x => x.AvgExecutionTime <= filter.MaxAvgTime);
+            }
+
+            var results = await query.ToListAsync();
+
+            return Ok(results);
+        }
+    }
+}
---
+++ DTO/ResultFilterDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Web_Api_Timescale_Data.DTO
+{
+    public class ResultFilterDTO
+    {
+        public string? FileName { get; set; }
+
+        public DateTime? MinStartDate { get; set; }
+        public DateTime? MaxStartDate { get; set; }
+
+        public double? MinAvgValue { get; set; }
+        public double? MaxAvgValue { get; set; }
+
+        public double? MinAvgTime { get; set; }
+        public double? MaxAvgTime { get; set; }
+    }
+}
